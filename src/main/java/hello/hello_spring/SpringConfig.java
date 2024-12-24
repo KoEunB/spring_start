e0cf8aec9569ed1d96e09e468f481fc3e4c9e9d3
EunBi Ko <e_un_04@naver.com>
2024-12-24 20:43:08 +0900

    스프링 통합 테스트

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hello_spring;
 
 import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JdbcTemplateMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
@@ -14,9 +15,10 @@ import javax.xml.crypto.Data;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
-    @Autowired
+    private final DataSource dataSource;
+
     public SpringConfig(DataSource dataSource){
+
         this.dataSource = dataSource;
     }
     @Bean
@@ -25,9 +27,11 @@ public class SpringConfig {
         return new MemberService(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository(){
 
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
