8dc5252d348875a26a28d27fd153e484e26e1d2c
EunBi Ko <e_un_04@naver.com>
2024-12-30 01:15:30 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -2,38 +2,17 @@ package hello.hello_spring;
 
 import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
-
 @Configuration
 public class SpringConfig {
-
-    private final DataSource dataSource;
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em){
-        this.dataSource = dataSource;
-        this.em = em;
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
     @Bean
-    public MemberService memberService(){
-
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository(){
-
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 }
